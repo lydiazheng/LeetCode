fad7b0c9e9ebe923e0a7237a1ecdb007d3b3cef1
lydiazheng <lydiazheng0928@gmail.com>
2017-01-11 14:22:51 -0800

    add 191

---
+++ 191_NumberOf1Bits.cpp
@@ -0,0 +1,22 @@
+/*
+Write a function that takes an unsigned integer and 
+returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, 
+so the function should return 3.
+*/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        if(n == 0)  return 0;
+        for(int i = 0; i < 32; i++){
+            if(n&1 == 1) count++;
+            n = n>>1;
+        }
+        
+        return count;
+    }
+};
